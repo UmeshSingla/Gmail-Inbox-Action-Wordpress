3db84e6b08d37fa27593ff6798482a5c91f72ea1
Faishal Saiyed <saiyedfaishal@gmail.com>
2014-03-08 02:25:40 +0530

    Add phpdoc

--- app/GIAOptions.php
+++ app/GIAOptions.php
@@ -1,14 +1,26 @@
 <?php
 
+/**
+ * Class GIAOptions
+ */
 class GIAOptions {
+	/**
+	 *
+	 */
 	function __construct() {
 		add_action( 'admin_menu', array( $this, 'gia_admin_menu' ) );
 	}
 
+	/**
+	 *
+	 */
 	function gia_admin_menu() {
 		add_options_page( 'Google Inbox Action Instructions', 'Google Inbox Action', 'manage_options', 'google-inbox-action', array( $this, 'gia_settings_page' ) );
 	}
 
+	/**
+	 *
+	 */
 	function gia_settings_page() {
 		?>
 		<div class="wrap">
